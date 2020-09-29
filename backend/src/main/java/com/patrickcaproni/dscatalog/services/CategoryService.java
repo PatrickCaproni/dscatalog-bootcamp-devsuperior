075a067de9cfeb609ae62e72efa6c53e568062cd
PatrickCaproni <patrick.-m@hotmail.com>
2020-09-29 14:45:09 -0300

    DTO

--- backend/src/main/java/com/patrickcaproni/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/patrickcaproni/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.patrickcaproni.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.patrickcaproni.dscatalog.dto.CategoryDTO;
 import com.patrickcaproni.dscatalog.entities.Category;
 import com.patrickcaproni.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,10 @@ public class CategoryService {
 	public CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 }
