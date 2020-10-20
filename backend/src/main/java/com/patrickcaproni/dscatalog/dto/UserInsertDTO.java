0a1bbbee6638536aa60727d31c28129fbd67defa
PatrickCaproni <patrick.-m@hotmail.com>
2020-10-20 15:08:20 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/patrickcaproni/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/patrickcaproni/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.patrickcaproni.dscatalog.dto;
 
+import com.patrickcaproni.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
