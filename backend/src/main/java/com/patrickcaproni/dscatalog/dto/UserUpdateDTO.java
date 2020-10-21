c5e6e43ff8de131b1d842c2816a5c4263061f2ce
PatrickCaproni <patrick.-m@hotmail.com>
2020-10-21 10:49:14 -0300

    UserUpdateValidator

---
+++ backend/src/main/java/com/patrickcaproni/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.patrickcaproni.dscatalog.dto;
+
+import com.patrickcaproni.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+
+
+}
--- backend/src/main/java/com/patrickcaproni/dscatalog/services/UserService.java
+++ backend/src/main/java/com/patrickcaproni/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.patrickcaproni.dscatalog.dto.RoleDTO;
 import com.patrickcaproni.dscatalog.dto.UserDTO;
 import com.patrickcaproni.dscatalog.dto.UserInsertDTO;
+import com.patrickcaproni.dscatalog.dto.UserUpdateDTO;
 import com.patrickcaproni.dscatalog.entities.Role;
 import com.patrickcaproni.dscatalog.entities.User;
 import com.patrickcaproni.dscatalog.repositories.RoleRepository;
@@ -58,7 +59,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getOne(id);
 			copyDtoToEntity(dto, entity);
