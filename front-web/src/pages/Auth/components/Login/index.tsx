821376e2dfd3fcbd9f6d9ce4782a06acecc8a966
PatrickCaproni <patrick.-m@hotmail.com>
2020-11-02 11:46:24 -0300

    Apply login form validations

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -14,7 +14,7 @@ type FormData = {
 
 const Login = () => {
 
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, errors } = useForm<FormData>();
     const [hasError, setHasError] = useState(false);
     const history = useHistory();
 
@@ -39,18 +39,38 @@ const Login = () => {
                 </div>
             )}
             <form onSubmit={handleSubmit(onSubmit)} className="login-form">
-              <input type="email"
-                name="username"
-                className="form-control input-base margin-bottom-30"
-                placeholder="Email"
-                ref={register({ required: true })}
-               /> 
-              <input type="password"
-                name="password"
-                className="form-control input-base"
-                placeholder="Senha"
-                ref={register({ required: true })}
-               /> 
+              <div className="margin-bottom-30">
+                <input type="email"
+                    name="username"
+                    className={`form-control input-base ${errors.username ? 'is-invalid' : '' }`}
+                    placeholder="Email"
+                    ref={register({
+                        required: "Campo obrigatório",
+                        pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "Email inválido"
+                        }
+                    })}
+                /> 
+                {errors.username && (
+                    <div className="invalid-feedback d-block">
+                        {errors.username.message}
+                    </div>
+                )}
+               </div>
+              <div>
+                <input type="password"
+                    name="password"
+                    className={`form-control input-base ${errors.password ? 'is-invalid' : '' }`}
+                    placeholder="Senha"
+                    ref={register({ required: "Campo obrigatório" })}
+                /> 
+                {errors.password && (
+                     <div className="invalid-feedback d-block">
+                         {errors.password.message}
+                    </div>
+                )}
+               </div>
                <Link to="/admin/auth/recover" className="login-link-recover">
                 Esqueci a senha?
                </Link>
